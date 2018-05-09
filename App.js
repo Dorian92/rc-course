2ffcddb0645d71efbfccc99e966d44e0109671a3
Dorian Sabo <dorian.sabo@brightmarbles.io>
2018-05-09 16:32:45 +0200

    Adding a Modal

--- App.js
+++ App.js
@@ -1,44 +1,71 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
 
-import PlaceInput from './src/components/PlaceInput/PlaceInput';
-import PlaceList from './src/components/PlaceList/PlaceList';
+import PlaceInput from "./src/components/PlaceInput/PlaceInput";
+import PlaceList from "./src/components/PlaceList/PlaceList";
+import PlaceDetail from "./src/components/PlaceDetail/PlaceDetail";
 
-
-export default class App extends React.Component {
+export default class App extends Component {
   state = {
-    places: []
+    places: [],
+    selectedPlace: null
   };
 
   placeAddedHandler = placeName => {
-      this.setState(prevState => {
-        return {
-            places: prevState.places.concat({
-              key: Math.random(), 
-              name: placeName,
-              image: {
-                uri: "http://allpicts.in/download/13484/Nature_Wallpaper_with_Beautiful_White_Lotus_Flower.jpg/"
-              }
-          })
-        };
-      })
-  }
-
-  placeDeletedHandler = key => {
     this.setState(prevState => {
       return {
-        places: prevState.places.filter(place => {
-          return place.key !== key;
+        places: prevState.places.concat({
+          key: Math.random(),
+          name: placeName,
+          image: {
+            uri:
+              "https://c1.staticflickr.com/5/4096/4744241983_34023bf303_b.jpg"
+          }
         })
       };
     });
-  }
+  };
+
+  placeDeletedHandler = () => {
+    this.setState(prevState => {
+      return {
+        places: prevState.places.filter(place => {
+          return place.key !== prevState.selectedPlace.key;
+        }),
+        selectedPlace: null
+      };
+    });
+  };
+
+  modalClosedHandler = () => {
+    this.setState({
+      selectedPlace: null
+    });
+  };
+
+  placeSelectedHandler = key => {
+    this.setState(prevState => {
+      return {
+        selectedPlace: prevState.places.find(place => {
+          return place.key === key;
+        })
+      };
+    });
+  };
 
   render() {
     return (
       <View style={styles.container}>
-       <PlaceInput onPlaceAdded={this.placeAddedHandler} />
-       <PlaceList places={this.state.places} onItemDeleted={this.placeDeletedHandler}/>
+        <PlaceDetail
+          selectedPlace={this.state.selectedPlace}
+          onItemDeleted={this.placeDeletedHandler}
+          onModalClosed={this.modalClosedHandler}
+        />
+        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
+        <PlaceList
+          places={this.state.places}
+          onItemSelected={this.placeSelectedHandler}
+        />
       </View>
     );
   }
@@ -48,8 +75,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 26,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start"
   }
 });
