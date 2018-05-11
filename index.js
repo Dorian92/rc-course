51ee27968b3182bf239f860419d6544a7513d204
Dorian Sabo <dorian.sabo@brightmarbles.io>
2018-05-11 15:17:35 +0200

    Connecting Screens to Redux

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 import App from './App';
+import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux';
+import App from './App';
+import configureStore from './src/store/configureStore';
+
+const store = configureStore();
+
+const RNRedux = () => (
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
+
+AppRegistry.registerComponent('rncourse', () => RNRedux);
 
 
