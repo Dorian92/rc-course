7ba78976a4f237c3e8869f21de9dbfa94ace22e4
Dorian Sabo <dorian.sabo@brightmarbles.io>
2018-05-11 16:28:29 +0200

    Pushing Pages (Navigating  Forwards)

--- src/screens/FindPlace/FindPlace.js
+++ src/screens/FindPlace/FindPlace.js
@@ -5,10 +5,24 @@ import { connect } from 'react-redux';
 import PlaceList from '../../components/PlaceList/PlaceList';
 
 class FindPlaceScreen extends Component {
+    itemSelectedHandler = key => {
+        const slePlace = this.props.places.find(places => {
+            return places.key === key;
+        });
+
+        this.props.navigator.push({
+            screen: "awesome-places.PlaceDetailScreen",
+            title: slePlace.name,
+            passProps: {
+                selectedPlace: slePlace
+            }
+        });
+    }
+
     render() {
         return (
             <View>
-                <PlaceList places={this.props.places} />
+                <PlaceList places={this.props.places} onItemSelected={this.itemSelectedHandler} />
             </View>
         )
     }
