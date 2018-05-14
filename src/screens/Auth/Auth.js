f03a602d16bf812ce1eb179c9ad26a41bfae3d1c
Dorian Sabo <dorian.sabo@brightmarbles.io>
2018-05-14 14:41:02 +0200

    Synthetic Cascading of Styles

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -14,9 +14,9 @@ class AuthScreen extends Component {
                 <Text>Please Log In</Text>
                 <Button title="Switch to Login" />
                     <View style={styles.inputContainer}>
-                        <DefaultInput placeholder="Your E-Mail Adress" />
-                        <DefaultInput placeholder="Password" />
-                        <DefaultInput placeholder="Confirm Password" />
+                        <DefaultInput placeholder="Your E-Mail Adress" style={styles.input} />
+                        <DefaultInput placeholder="Password" style={styles.input} />
+                        <DefaultInput placeholder="Confirm Password" style={styles.input} />
                     </View>
                 <Button title="Submit" onPress={this.loginHandler}></Button>
             </View>
@@ -32,6 +32,10 @@ const styles = StyleSheet.create({
     },
     inputContainer: {
         width: "80%"
+    },
+    input: {
+        backgroundColor: "#eee",
+        borderColor: "#bbb"
     }
 })
 
