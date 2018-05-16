c6f9d3e080497cb5d0df6c2fea934ab8750d1b80
Dorian Sabo <dorian.sabo@brightmarbles.io>
2018-05-16 11:39:20 +0200

    Adding Custom Validation Logic

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -15,6 +15,7 @@ import HeadingText from '../../components/UI/HeadingText/HeadingText';
 import MainText from '../../components/UI/MainText/MainText';
 import ButtonWithBackGround from '../../components/UI/ButtonWithBackground/ButtonWithBackground';
 import backgroundImage from '../../assets/background.jpg';
+import validate from '../../utility/validation';
 
 
 class AuthScreen extends Component {
@@ -64,13 +65,33 @@ class AuthScreen extends Component {
     }
 
     updateInputState = (key, value) => {
+        let connectedValue = {};
+        if (this.state.controls[key].validationRules.equalTo) {
+            const equalControl = this.state.controls[key].validationRules.equalTo;
+            const equalValue = this.state.controls[equalControl].value;
+            connectedValue = {
+                ...connectedValue,
+                equalTo: equalValue
+            }
+        }
+        if (key === 'password') {
+            connectedValue = {
+                ...connectedValue,
+                equalTo: value
+            }
+        }
         this.setState(prevState => {
             return {
                 controls: {
                     ...prevState.controls,
+                    confirmPassword: {
+                        ...prevState.controls.confirmPassword,
+                        valid: key === 'password' ? validate(prevState.controls.confirmPassword.value, prevState.controls.confirmPassword.validationRules, connectedValue) : prevState.controls.confirmPassword.valid
+                    },
                     [key]: {
                         ...prevState.controls[key],
-                        value: value
+                        value: value,
+                        valid: validate(value, prevState.controls[key].validationRules, connectedValue)
                     }
                 }
             }
@@ -113,6 +134,8 @@ class AuthScreen extends Component {
                                 <DefaultInput 
                                     placeholder="Password" 
                                     style={styles.input} 
+                                    value={this.state.controls.password.value}
+                                    onChangeText={(val) => this.updateInputState('password', val)}
                                 />
                             </View>
                             <View 
@@ -122,7 +145,9 @@ class AuthScreen extends Component {
                             }>
                                 <DefaultInput 
                                     placeholder="Confirm Password" 
-                                    style={styles.input} 
+                                    style={styles.input}
+                                    value={this.state.controls.confirmPassword.value}
+                                    onChangeText={(val) => this.updateInputState('confirmPassword', val)} 
                                 />
                             </View>
                         </View>
