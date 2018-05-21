5d3f4bbdf45a8823c8bcb1e722994719e3cfc9ec
Dorian Sabo <dorian.sabo@brightmarbles.io>
2018-05-21 11:21:27 +0200

    Stroing the Remaining Data

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,10 +1,6 @@
 import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
 export const addPlace = (placeName, location, image) => {
     return dispatch => {
-        const placeData = {
-            name: placeName,
-            location: location
-        };
         fetch("https://us-central1-awesome-places-1526557212424.cloudfunctions.net/storeImage",{
             method: "POST",
             body: JSON.stringify({
@@ -14,18 +10,21 @@ export const addPlace = (placeName, location, image) => {
         .catch(err => console.log(err))
         .then(res => res.json())
         .then(parsedRes => {
+            const placeData = {
+                name: placeName,
+                location: location,
+                image: parsedRes.imageUrl
+            };
+            return fetch("https://awesome-places-1526557212424.firebaseio.com/places.json", {
+                method: "POST",
+                body: JSON.stringify(placeData)
+            })
+        })
+        .catch(err => console.log(err))
+        .then(res => res.json())
+        .then(parsedRes => {
             console.log(parsedRes);
         })
-
-        // fetch("https://awesome-places-1526557212424.firebaseio.com/places.json", {
-        //     method: "POST",
-        //     body: JSON.stringify(placeData)
-        // })
-        // .catch(err => console.log(err))
-        // .then(res => res.json())
-        // .then(parsedRes => {
-        //     console.log(parsedRes);
-        // })
     }
 };
 
