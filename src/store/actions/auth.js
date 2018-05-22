cd9c33d6c72f5fdd6d831fd4bc4a890a6fc11844
Dorian Sabo <dorian.sabo@brightmarbles.io>
2018-05-22 10:11:57 +0200

    Adding User Login

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -4,17 +4,50 @@ import startMainTabs from '../../screens/MainTabs/startMainTabs';
 
 export const tryAuth = (authData, authMode) => {
     return dispatch => {
-        if (authMode === 'login') {
-
-        } else {
-            dispatch(authSignup(authData));
+        dispatch(uiStartLoading());
+        const apiKey = "AIzaSyCO_QYDaVlh6s4UlB9_ur1Zp4ctKoF2l8Y"
+        let url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=" + apiKey;
+        if (authMode === 'signup') {
+            url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=" + apiKey;
         }
+        fetch(url,{
+            method: "POST",
+            body: JSON.stringify({
+                email: authData.email,
+                password: authData.password,
+                returnSecureToken: true
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            alert("Authentication faild, please try again");
+            dispatch(uiStopLoading());
+        })
+        .then(res => res.json())
+        .then(parsedRes => {
+            dispatch(uiStopLoading());
+            if(parsedRes.error){
+                alert("Authentication faild, please try again");
+            } else {
+                startMainTabs();
+            }
+        });
     };
 };
 
 export const authSignup = (authData) => {
     return dispatch => {
         dispatch(uiStartLoading());
+
+    };
+};
+
+export const authSignin = authData => {
+    return dispatch => {
+        
         fetch("https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCO_QYDaVlh6s4UlB9_ur1Zp4ctKoF2l8Y",{
             method: "POST",
             body: JSON.stringify({
