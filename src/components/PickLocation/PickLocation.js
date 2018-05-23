be473b135b85b3205b2d4d759b11a7863295e646
Dorian Sabo <dorian.sabo@brightmarbles.io>
2018-05-23 15:34:23 +0200

    Resetting the Share Place Screen

--- src/components/PickLocation/PickLocation.js
+++ src/components/PickLocation/PickLocation.js
@@ -2,15 +2,9 @@ import React, { Component } from 'react';
 import { View, Image, Button, StyleSheet, Text, Dimensions } from 'react-native';
 import MapView from 'react-native-maps';
 class PickLocation extends Component {
-   state = {
-       focusedLocation: {
-               latitude: 37.7900352,
-               longitude: -122.4013726,
-               latitudeDelta: 0.0122,
-               longitudeDelta: Dimensions.get('window').width / Dimensions.get('window').height * 0.0122
-       },
-       locationChosen: false
-   }
+componentWillMount() {
+    this.reset();
+}
 
    pickLocationHandler = event => {
      const coords = event.nativeEvent.coordinate;
@@ -53,6 +47,18 @@ class PickLocation extends Component {
     })
    }
 
+   reset = () => {
+       this.setState({
+            focusedLocation: {
+                latitude: 37.7900352,
+                longitude: -122.4013726,
+                latitudeDelta: 0.0122,
+                longitudeDelta: Dimensions.get('window').width / Dimensions.get('window').height * 0.0122
+        },
+        locationChosen: false
+       })
+   }
+
    render() {
       let marker = null;
 
@@ -63,6 +69,7 @@ class PickLocation extends Component {
            <View style={styles.container}>
                <MapView
                   initialRegion={this.state.focusedLocation}
+                  region={!this.state.locationChosen ? this.state.focusedLocation : null}
                   style={styles.map}
                   onPress={this.pickLocationHandler}
                   ref={ref => this.map = ref}
